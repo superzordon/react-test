7dcb1d83594136822e305aaebd14956fb5f429bc
superzordon <zordonclout@gmail.com>
2023-02-15 14:29:28 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+    const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
+    const [passwordSubmitted, setPasswordSubmitted] = useState(false);
+  return (
+      <div className="Maybe Check out the Sources Tab...">
+          {!isPasswordCorrect && <form
+              onSubmit={async (ev) => {
+                  ev.preventDefault();
+                  console.log("Getting warmer...")
+                  setPasswordSubmitted(true);
+                  // @ts-ignore
+                  if (ev.target.password.value === 'diamondhands') {
+                      setIsPasswordCorrect(true);
+                  } else {
+                  }
+              }}
+          >
+              <label>
+                  Password:
+                  <input type="text" name="password" />
+              </label>
+              <input type="submit" value="Submit" />
+          </form>}
+          {passwordSubmitted && !isPasswordCorrect && <p className='incorrect'>Incorrect Password</p>}
+          {isPasswordCorrect && <div className="hidden"/>}
+      </div>
+  );
+}
+
+export default App;
